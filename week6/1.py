75e4f47517ab470ddae30b26430883905e3133c4
rasoolsf <safari@rasool.id.ir>
2017-07-15 15:19:21 +0430

    ...

---
+++ week6/1.py
@@ -0,0 +1,6 @@
+# Type your code here
+i=1
+n=int(input("Please enter a number: "))
+while i <= n :
+	print (str(i)*i)
+	i = i + 1
---
+++ week6/2.py
@@ -0,0 +1,7 @@
+# Type your code here
+i = 1
+x = int(input("Please enter a number: "))
+arz = "*"*x
+while i <= x:
+	print (arz)
+	i = i + 1
