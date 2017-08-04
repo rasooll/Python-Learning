2bdb10238e65f3fd0cb6802b7dd561a1d75feac2
rasoolsf <safari@rasool.id.ir>
2017-08-04 18:20:25 +0430

    ...

---
+++ week6/methods/3.py
@@ -0,0 +1,15 @@
+#print ("university".index("iv"))
+
+#my_str = "this is a test"
+#print (my_str.count('s',5))
+
+#print ('test'.center(10,'x'))
+
+#my_str = "Hello"
+#print (my_str.center(9,'z'))
+
+#my_str = "CSE"
+#print (my_str.ljust(7,'x'))
+
+#my_string = 'hello'
+#print (my_string.rjust(8,"A"))
