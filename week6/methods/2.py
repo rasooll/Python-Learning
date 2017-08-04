012c2f7cbafadd3dae77bbb1c4a81368133bb222
rasoolsf <safari@rasool.id.ir>
2017-08-04 18:20:20 +0430

    ...

---
+++ week6/methods/2.py
@@ -0,0 +1,12 @@
+#print ("abcdef".find("e"))
+
+#x="Mississippi"
+#print (x.rfind('si',3))
+
+#my_string = "knickknack"
+#print (my_string.rfind('k',3,-2))
+
+#my_str='Engineering'
+#print (my_str.replace('e','x')) 
+
+#print ("Mississippi".replace('i','z',2))
