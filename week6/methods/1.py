3febdb7ccc874bd4be9e38c5de852e9c5fac0acf
rasoolsf <safari@rasool.id.ir>
2017-08-04 18:20:14 +0430

    ...

---
+++ week6/methods/1.py
@@ -0,0 +1,15 @@
+#print ("hello".isalpha())
+
+#my_str = "hello there"
+#print (my_str.isalpha())
+
+#my_str = "hello12"
+#print (my_str.isalpha())
+
+#my_str = "CSE1309"
+#print (my_str.isalnum())
+
+#my_str = "CSE-1309"
+#print (my_str.isalnum())
+
+#print ("bird".endswith("ir"))
