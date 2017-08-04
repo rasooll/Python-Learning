e3545519cd153eb65a215df83b51d4503ad8b149
rasoolsf <safari@rasool.id.ir>
2017-08-04 18:20:33 +0430

    ...

---
+++ week6/methods/4.py
@@ -0,0 +1,16 @@
+#x="oops too"
+#print (x.strip("o")) 
+
+#my_str = "oops too"
+#print (my_str.lstrip('o'))
+
+#my_str = "oops too"
+#print (my_str.rstrip('o'))
+
+#my_str = "hello hello"
+#print (my_str.rstrip('h'))
+
+#print ("NO".join("test"))
+
+#x= "test".join(["A","B","C"])
+#print (x) 
