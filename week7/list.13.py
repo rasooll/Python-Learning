1174e0c475855be02505719d1b5e573ddcee6ac5
rasoolsf <safari@rasool.id.ir>
2017-08-07 15:33:07 +0430

    ...

---
+++ week7/list.13.py
@@ -0,0 +1,15 @@
+def _product_of_two_vectors_sample_(a, b):
+    if len(a[0]) != len(b):
+        return None
+    # Create the result matrix and fill it with zeros
+    output_list=[]
+    temp_row=len(b[0])*[0]
+    for r in range(len(a)):
+        output_list.append(temp_row[:])
+    for row_index in range(len(a)):
+        for col_index in range(len(b[0])):
+            sum=0
+            for k in range(len(a[0])):
+                sum=sum+a[row_index][k]*b[k][col_index]
+            output_list[row_index][col_index]=sum
+    return output_list
