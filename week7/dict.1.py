2aad8505c1be54abd066672d2ad622da18144ad1
rasoolsf <safari@rasool.id.ir>
2017-08-08 15:33:51 +0430

    ...

---
+++ week7/dict.1.py
@@ -0,0 +1,12 @@
+numbers={"one": 1, "two": 2, "three": 3}
+print (numbers["two"])
+
+numbers={"one": "uno", "two": "dos", "three": "tres"}
+print (numbers["three"])
+
+numbers={"one": 1, "two": 2}
+print (numbers[2])
+
+numbers={"one": "uno", "two": "dos", "three": "tres"}
+print ("two" in numbers)
+
