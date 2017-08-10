d9935855866d6aba63478f0c83042c305227a567
rasoolsf <safari@rasool.id.ir>
2017-08-10 23:12:59 +0430

    ...

--- Taklif3/1.py
+++ Taklif3/1.py
@@ -1,12 +1,14 @@
 def find_word_vertical(crosswords,word):
-	z=[list(i) for i in zip(*crosswords)]   
-	for rows in z:          
-		row_index = z.index(rows)
-		single_row = ''.join(rows)      
-		column_index = single_row.find(word)        
-		if column_index >= 0:
-			break
-	return([column_index, row_index])
+    if not crosswords or not word:
+        return None    
+    number_of_columns=len(crosswords[0])
+    for col_index in range (number_of_columns):
+        temp_str=''
+        for row_index in range(len(crosswords)):
+            temp_str=temp_str+crosswords[row_index][col_index]
+        if temp_str.find(word)>=0:
+            return [temp_str.find(word),col_index]
+    return None
 crosswords=[['s','d','o','g'],['c','u','c','m'],['a','c','a','t'],['t','e','t','k']]
 word='cat'
 print(find_word_vertical(crosswords,word))
